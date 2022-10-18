bd9364e5478dcb5618de56549c39002a916fb6d6
surya2info <surya2info@gmail.com>
2022-10-18 11:05:54 +0530

    java program

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main{
+
+public static void main(String[] args){
+
+System.out.println("Hello world");
+System.out.println("line2");
+System.out.println("line3");
+System.out.println("line4");
+System.out.println("line5");
+System.out.println("line6");
+System.out.println("line7");
+System.out.println("line8");
+
+}
+
+}
